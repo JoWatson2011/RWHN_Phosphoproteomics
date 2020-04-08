a2fdbe4dce165fcf4523e68d5a38ffc85fcf5e2b
Jo <jowatson2011@hotmail.co.uk>
2020-04-08 10:55:54 +0100

    Split data

--- src/CaseStudy_Ruprecht2017.R
+++ src/CaseStudy_Ruprecht2017.R
@@ -29,17 +29,22 @@ ruprecht_sty <- data.table::fread(input = "data/Ruprecht_STY_2017.csv",
 ) %>% 
   mutate(`Gene names` = gsub(";.*", "", `Gene names`),
          id = paste0(`Gene names`, "_", `Amino acid` ,`Position`)) %>% 
-  filter(!duplicated(id)) %>% 
-  rename(`Gene names` = "gene.symbol")
-colnames(ruprecht_sty) <- gsub(" ", "_", colnames(ruprecht_sty))
+  filter(!duplicated(id)) 
+colnames(ruprecht_sty) <- gsub(" ", ".", colnames(ruprecht_sty))
+colnames(ruprecht_sty)[grep("FDR.<.1%",
+                            colnames(ruprecht_sty))] <- substr(grep("FDR.<.1%", colnames(ruprecht_sty),
+                                                                      value = T),
+                                                                 8, 
+                                                                 21)
+colnames(ruprecht_sty) <- gsub("/", "", colnames(ruprecht_sty))
 
 
 # PARENTAL + LAP M/L: Filter NAs and non significant
 par_lap <- ruprecht_sty %>%
-  dplyr::select(id, grep("M/L", colnames(.))) %>%
-  filter(`t-test_SignificantM/L_(FDR_<_1%)` == "+" ) %>%
+  dplyr::select(id, grep("ML", colnames(.))) %>%
+  filter(SignificantML == "+" ) %>%
   na.omit() %>%
-  dplyr::select(-`t-test_SignificantM/L_(FDR_<_1%)`
+  dplyr::select(-SignificantML
   ) %>%
   pivot_longer(-id,
                names_to = "rep",
@@ -53,11 +58,11 @@ par_lap <- ruprecht_sty %>%
 
 
 # RESISTANT  + LAP H/L: Filter NAs
-par_lap <- ruprecht_sty %>%
-  dplyr::select(id, grep("H/L", colnames(.))) %>%
-  filter(`t-test_SignificantH/L_(FDR_<_1%)` == "+" ) %>%
+res_lap <- ruprecht_sty %>%
+  dplyr::select(id, grep("HL", colnames(.))) %>%
+  filter(SignificantHL == "+" ) %>%
   na.omit() %>%
-  dplyr::select(-`t-test_SignificantH/L_(FDR_<_1%)`
+  dplyr::select(-SignificantHL
   ) %>%
   pivot_longer(-id,
                names_to = "rep",
@@ -71,59 +76,88 @@ par_lap <- ruprecht_sty %>%
 
 # ALL COND. : Filter data with NA in an experimental condition
 tot_lap <- ruprecht_sty %>%
-  dplyr::select(id, grep("H[M]/L", colnames(.))) %>%
-filter(`t-test_SignificantM/L_(FDR_<_1%)` == "+" |
-        `t-test_SignificantH/L_(FDR_<_1%)` == "+") %>%
-  filter(`t-test_SignificantM/L_(FDR_<_1%)` == "+" ) %>%
-#  filter_missing(allowed = 1,  colnms = "^[M]/L") %>%
-  na.omit() %>%
-  # dplyr::select(-c(`t-test_SignificantM/L_(FDR_<_1%)`,
-  #                  `t-test_SignificantH/L_(FDR_<_1%)`)
-  #               ) %>%
-  dplyr::select(-`t-test_SignificantM/L_(FDR_<_1%)`
-  ) %>%
-  pivot_longer(-id,
+  dplyr::select(id, grep("[HM]L", colnames(.))) %>%
+  filter(SignificantML == "+" |
+           SignificantHL == "+") %>%
+  filter_missing(allowed = 1,  colnms = "^[HM]L_") %>%
+  mutate_if(is.numeric, imputeTruncNorm) %>% 
+  # dplyr::select(-c(SignificantML,
+  #                  SignificantHL)
+  # ) %>%
+  pivot_longer(-c(id, SignificantML, SignificantHL),
                names_to = "rep",
                values_to = "ratio") %>%
   mutate(rep = gsub("_R[1234]$", "", rep)) %>%
   group_by(id, rep) %>%
-  summarise(ratio = median(ratio, na_rm = T)) %>%
-  pivot_wider(id_cols = id,
+  mutate(ratio = median(ratio, na_rm = T)) %>%
+  unique() %>% 
+  pivot_wider(#id_cols = id,
               names_from = rep,
               values_from = ratio)
 
 # Cluster based on dynamics of phosphorylated sites
+wss <- data.frame(type = c(rep("All significant", 15),
+                           rep("Just * parental", 15), 
+                           rep("Just * resistant", 15)),
+                  cl = rep(1:15, 3),
+                  wss = c(sapply(1:15, 
+                                 function(k){kmeans(tot_lap[,4:5], 
+                                                    k, nstart=50,
+                                                    iter.max = 15 )$tot.withinss}),
+                          sapply(1:15, 
+                                 function(k){kmeans(par_lap[,2], 
+                                                    k, nstart=50,
+                                                    iter.max = 15 )$tot.withinss}),
+                          sapply(1:15, 
+                                 function(k){kmeans(res_lap[,2], 
+                                                    k, nstart=50,
+                                                    iter.max = 15 )$tot.withinss})),
+                  stringsAsFactors = F)
+ggplot(wss, aes(x = cl, y = wss, color = type)) +
+  geom_point() + 
+  geom_line() +
+  xlab("Number of clusters K") +
+  ylab("Total within-clusters sum of squares") +
+  scale_color_discrete()
 
-# par_lap_fcm <- fuzzyC(par_lap, 3)
-# 
-# par_lap_hclust <- hclust(dist(par_lap[,2:3]))
-# plot(par_lap_hclust)
-# par_lap_cl <- cutree(par_lap_hclust, k = 5)
 
-par_lap_cl <- kmeans(par_lap[,2], 4)$cluster
-par_lap_cl <- kmeans(par_lap[,2:3], 4)$cluster
+par_lap_cl <- kmeans(par_lap[,2], 2)$cluster
 names(par_lap_cl) <- par_lap$id
 
-#res_lap_fcm <- fuzzyC(res_lap, 6)
+res_lap_cl <- kmeans(res_lap[,2], 4)$cluster
+names(res_lap_cl) <- res_lap$id
+
+tot_lap_cl <- kmeans(tot_lap[,4:5], 5)$cluster
+names(tot_lap_cl) <- tot_lap$id
+
 
 # Construct heterogeneous network
 par_mlnw <- constructHetNet(ruprecht_sty, par_lap, par_lap_cl, modules = T)
-#tgf_mlnw <- constructHetNet(ruprecht_sty, res_lap, res_lap_fcm)
+res_mlnw <- constructHetNet(ruprecht_sty, res_lap, res_lap_cl, modules = T)
+tot_mlnw <- constructHetNet(ruprecht_sty, tot_lap, tot_lap_cl, modules = T)
 
 ## Run RWHN algorithm
 # Recommend to run overnight or on HPC
 
-par_mlnw$edgelists <- lapply(par_mlnw$edgelists, function(i)
-  i %>% filter_all(any_vars(. != "negative regulation of transcription by RNA polymerase II"))
-)
-par_mlnw$v <- par_mlnw$v[par_mlnw$v$v != "negative regulation of transcription by RNA polymerase II",]
+# par_mlnw$edgelists <- lapply(par_mlnw$edgelists, function(i)
+#   i %>% filter_all(any_vars(. != "negative regulation of transcription by RNA polymerase II"))
+# )
+# par_mlnw$v <- par_mlnw$v[par_mlnw$v$v != "negative regulation of transcription by RNA polymerase II",]
 
-seed_l <- lapply(1:max(par_lap_cl), function(i){
+seed_par <- lapply(1:max(par_lap_cl), function(i){
   c(names(par_lap_cl[par_lap_cl == i]))
 })
 
-start <- Sys.time()
-rwhn_par <- lapply(seed_l, function(s){
+seed_res <- lapply(1:max(res_lap_cl), function(i){
+  c(names(res_lap_cl[res_lap_cl == i]))
+})
+
+seed_tot <- lapply(1:max(tot_lap_cl), function(i){
+  c(names(tot_lap_cl[tot_lap_cl == i]))
+})
+
+paste("start par", Sys.time())
+rwhn_par <- lapply(seed_par, function(s){
   calculateRWHN(edgelists = par_mlnw$edgelists,
                 #verti = par_mlnw$v[par_mlnw$v$v != "negative regulation of transcription by RNA polymerase II",],
                 verti = par_mlnw$v,
@@ -134,8 +168,35 @@ rwhn_par <- lapply(seed_l, function(s){
                 weight_yz = 0.7) %>%
     filter(name %in% par_mlnw$v[par_mlnw$v$layer=="func",]$v)
 })
+
+paste("start res", Sys.time())
+rwhn_res <- lapply(seed_res, function(s){
+  calculateRWHN(edgelists = res_mlnw$edgelists,
+                #verti = res_mlnw$v[res_mlnw$v$v != "negative regulation of transcription by RNA polymerase II",],
+                verti = res_mlnw$v,
+                seeds = s,
+                transitionProb = 0.7,
+                restart = 0.7,
+                weight_xy = 0.3,
+                weight_yz = 0.7) %>%
+    filter(name %in% res_mlnw$v[res_mlnw$v$layer=="func",]$v)
+})
+
+paste("start tot", Sys.time())
+rwhn_tot <- lapply(seed_tot, function(s){
+  calculateRWHN(edgelists = tot_mlnw$edgelists,
+                #verti = tot_mlnw$v[tot_mlnw$v$v != "negative regulation of transcription by RNA polymerase II",],
+                verti = tot_mlnw$v,
+                seeds = s,
+                transitionProb = 0.7,
+                restart = 0.7,
+                weight_xy = 0.3,
+                weight_yz = 0.7) %>%
+    filter(name %in% tot_mlnw$v[tot_mlnw$v$layer=="func",]$v)
+})
+
 end <- Sys.time()
-  saveRDS(rwhn_par, "results/data/rwhn_ruprecht_clusters.rds")
+#saveRDS(rwhn_par, "results/data/rwhn_ruprecht_clusters.rds")
 
 
 

e72eb4dd5fe8d3e3dd5ab3d94c20e5e5353513b4
jowatson2011 <joanne.watson-5@postgrad.manchester.ac.uk>
2020-05-05 18:00:36 +0100

    tidy figures

--- src/functions/dotplot_gg.R
+++ src/functions/dotplot_gg.R
@@ -37,7 +37,8 @@ dotplot_gg <- function(rwhn, seed_names = NULL, n_terms = 20, remove_common = F,
     geom_count(aes(color = rank_flt), size = size) +
     theme_bw() +
     scale_y_discrete(name = "Cluster") +
-    scale_color_distiller(name = "RWHN Rank", palette = col) +
+    scale_color_distiller(name = "RWHN Rank", palette = col,
+                          guide = guide_colorbar(reverse = T)) +
     theme(axis.title.x = element_blank(),
           axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
           plot.margin = margin(10,10,10,160)
